f9670e4006652a7fb80b88547f8b0c704180c0a6
Anderson Martins <atmmoreira.rj@gmail.com>
2023-06-13 07:48:20 -0300

    feature: insert routes and highlight links

---
+++ js/script.js
@@ -0,0 +1,41 @@
+const global = {
+  currentPage: window.location.pathname,
+};
+
+// Highlight Active Links
+function highlightActiveLink() {
+  const links = document.querySelectorAll('.nav-link');
+  links.forEach(link => {
+    if (link.getAttribute('href') === global.currentPage) {
+      link.classList.add('active');
+    }
+  })
+}
+
+// Init App
+function Init() {
+  switch (global.currentPage) {
+    case '/':
+    case '/index.html':
+      console.log('Home');
+      break;
+    case '/shows.html':
+      console.log('Shows');
+      break;
+    case '/movie-details.html':
+      console.log('Movie Details');
+      break;
+    case '/tv-details.html':
+      console.log('TV Details');
+      break;
+    case '/search.html':
+      console.log('Search');
+      break;
+  }
+
+  // Invoke functions
+  highlightActiveLink();
+}
+
+// Add Event Listener
+document.addEventListener('DOMContentLoaded', Init);
