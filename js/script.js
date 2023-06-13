18fc89c9f25547ee656a636f8303e4951d6722c8
Anderson Martins <atmmoreira.rj@gmail.com>
2023-06-13 08:44:24 -0300

    feature: display movies on HomePage

--- js/script.js
+++ js/script.js
@@ -2,14 +2,49 @@ const global = {
   currentPage: window.location.pathname,
 };
 
+async function displayPopularMovies() {
+  const { results } = await fetchAPIData('movie/popular');
+  console.log(results);
+  results.forEach(movie => {
+    const div = document.createElement('div');
+    div.classList.add('card');
+    div.innerHTML = `
+      <a href="movie-details.html?id=${movie.id}">
+       ${
+          movie.poster_path
+          ? `<img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" class="card-img-top" alt="${movie.title}" />`
+          : `<img src="images/no-image.jpg" class="card-img-top" alt="${movie.title}" />`
+        }
+      </a>
+      <div class="card-body">
+        <h5 class="card-title">${movie.title}</h5>
+        <p class="card-text">
+          <small class="text-muted">Release: ${movie.release_date}</small>
+        </p>
+      </div>
+    `;
+
+    document.querySelector('#popular-movies').appendChild(div)
+  });
+}
+
+// Fetch data from TMDB API
+async function fetchAPIData(endpoint) {
+  const API_KEY = '3a6dacbb2c787551536f07cfc546eba1';
+  const API_URL = 'https://api.themoviedb.org/3/';
+  const response = await fetch(`${API_URL}${endpoint}?api_key=${API_KEY}`);
+  const data = await response.json();
+  return data;
+}
+
 // Highlight Active Links
 function highlightActiveLink() {
   const links = document.querySelectorAll('.nav-link');
-  links.forEach(link => {
+  links.forEach((link) => {
     if (link.getAttribute('href') === global.currentPage) {
       link.classList.add('active');
     }
-  })
+  });
 }
 
 // Init App
@@ -17,7 +52,7 @@ function Init() {
   switch (global.currentPage) {
     case '/':
     case '/index.html':
-      console.log('Home');
+      displayPopularMovies();
       break;
     case '/shows.html':
       console.log('Shows');
